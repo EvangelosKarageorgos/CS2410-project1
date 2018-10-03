5e59ae78b629894cc7f205f8417ea92901696316
xiaotingli <xiaotingli@fq-wifi-10-215-183-3.wireless.pitt.edu>
2018-10-03 12:54:23 -0400

    fixed bugs and read config works now

--- Tomasulo/CPU/ReadConfig.c
+++ Tomasulo/CPU/ReadConfig.c
@@ -7,6 +7,7 @@
 void read_configfile(char *fileName) {
     char *line = (char *) malloc (sizeof(char) * MAX_LINE);
     char *tempLine = (char *) malloc (sizeof(char) * MAX_LINE);
+    char *temp = (char *) malloc (sizeof(char) * MAX_LINE);
 
     size_t len = 0;
     ssize_t read;
@@ -20,44 +21,64 @@ void read_configfile(char *fileName) {
 
     while ((read = getline(&line, &len, fp)) != -1) { //loop to read file line by line and tokenize
         strcpy (tempLine, line);
-
+//        printf("%s", tempLine);
         if ((tempLine = strtok(tempLine, WHITE_SPACE)) == NULL || *tempLine == 0) {
             continue;
         }
-
-        if ((line = strtok(line, NF_VALUE)) == NULL || *line == 0) {
-            continue;
-        } else {
-            line = strtok(line, WHITE_SPACE);
-            NF = line[1];
+        char* line_ptr = NULL;
+        if ((line_ptr = strchr(line, 'F')) != NULL && *line_ptr != 0) {
+       //     continue;
+       // } else {
+            tempLine = strtok(line_ptr, WHITE_SPACE);
+            tempLine = strtok(NULL,",");
+//            printf("%s", tempLine);
+            strcpy(temp, tempLine);
+//            printf("%s\n", temp);
+            config -> NF = atoi(temp);
+            printf("%d\n", config -> NF);
         }
 
-        if ((line = strtok(line, 'NF')) == NULL || *line == 0) {
-            continue;
-        } else {
-            line = strtok(line, WHITE_SPACE);
-            NI = line[1];
+        if ((line_ptr = strchr(line, 'I')) != NULL && *line_ptr != 0) {
+            tempLine = strtok(line_ptr, WHITE_SPACE);
+            tempLine = strtok(NULL,",");
+//            printf("%s", tempLine);
+            strcpy(temp, tempLine);
+//            printf("%s\n", temp);
+            config -> NI = atoi(temp);
+            printf("%d\n", config -> NI);
+
         }
 
-        if ((line = strtok(line, 'NI')) == NULL || *line == 0) {
-            continue;
-        } else {
-            line = strtok(line, WHITE_SPACE);
-            NW = line[1];
+        if ((line_ptr = strchr(line, 'W')) != NULL && *line_ptr != 0) {
+            tempLine = strtok(line_ptr, WHITE_SPACE);
+            tempLine = strtok(NULL,",");
+//            printf("%s", tempLine);
+            strcpy(temp, tempLine);
+//            printf("%s\n", temp);
+            config -> NW = atoi(temp);
+            printf("%d\n", config -> NW);
         }
 
-        if ((line = strtok(line, 'NR')) == NULL || *line == 0) {
-            continue;
-        } else {
-            line = strtok(line, WHITE_SPACE);
-            NR = line[1];
+        if ((line_ptr = strchr(line, 'R')) != NULL && *line_ptr != 0) {
+            tempLine = strtok(line_ptr, WHITE_SPACE);
+            tempLine = strtok(NULL,",");
+//            printf("%s", tempLine);
+            strcpy(temp, tempLine);
+//            printf("%s\n", temp);
+            config -> NR = atoi(temp);
+            printf("%d\n", config -> NR);
+
         }
 
-        if ((line = strtok(line, 'NB')) == NULL || *line == 0) {
-            continue;
-        } else {
-            line = strtok(line, WHITE_SPACE);
-            NB = line[1];
+        if ((line_ptr = strchr(line, 'B')) != NULL && *line_ptr != 0) {
+            tempLine = strtok(line_ptr, WHITE_SPACE);
+            tempLine = strtok(NULL,",");
+//            printf("%s", tempLine);
+            strcpy(temp, tempLine);
+//            printf("%s\n", temp);
+            config -> NB = atoi(temp);
+            printf("%d\n", config -> NB);
+
         }
 
         line = (char *) malloc (sizeof(char) * MAX_LINE);
