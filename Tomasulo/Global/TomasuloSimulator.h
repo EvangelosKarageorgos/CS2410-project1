5f2d0ea11063ca24e6ec451d9e76bad59b21a8a4
xiaotingli <xiaotingli@fq-wifi-10-215-183-3.wireless.pitt.edu>
2018-10-03 10:42:37 -0400

    add macro for reservation stations

--- Tomasulo/Global/TomasuloSimulator.h
+++ Tomasulo/Global/TomasuloSimulator.h
@@ -25,6 +25,14 @@
 #define LABEL_TERMINATOR_CHAR ':'
 #define MEMORY_LABEL "Mem"
 #define MEMORY_SEPARATOR " )(=\n"
+#define RESERV_INT_SIZE    4
+#define RESERV_MULT_SIZE    2
+#define RESERV_LOAD_BUFFER_SIZE    2
+#define RESERV_STORE_BUFFER_SIZE    2
+#define RESERV_FPADD_SIZE    3
+#define RESERV_FPMULT_SIZE 4
+#define RESERV_FPDIVE_SIZE 2
+#define RESERV_BU_SIZE 2
 
 #define MAX_LINE  4096
 
